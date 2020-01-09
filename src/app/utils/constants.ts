393d6aa735ebe647341667b081e45a09aebe25e6
Fernando Arias <edfernando.arias@outlook.com>
2020-01-09 00:44:31 -0500

    Cambio seguridad url

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 export class ConstantsAplicacion {
 
-  public static SOURCE = 'aHR0cDovL2dlbmVyaWMtYmFja2VuZC1zcG90aWZ5Lmhlcm9rdWFwcC5jb20vc3BvdGlmeS9l' +
-    'ZjdkZWYyYWJhMTY0ZjA0ODdmMjY0MGVmN2MzNTNkMC84MmQ1NTYxYTc5Mzk0YWFmODA3MDFmYWFkNDZmYWZhMw==';
+  public static SOURCE = 'aHR0cHM6Ly9nZW5lcmljLWJhY2tlbmQtc3BvdGlmeS5oZXJva3VhcHAuY29tL3N' +
+  'wb3RpZnkvZWY3ZGVmMmFiYTE2NGYwNDg3ZjI2NDBlZjdjMzUzZDAvODJkNTU2MWE3OTM5NGFhZjgwNzAxZmFhZDQ2ZmFmYTM=';
 
   public static TOKEN = 'token';
 }
